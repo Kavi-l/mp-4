68f39e861c0008496949cd6c28b773fee4329467
Kavi Lu <kavilu@bu.edu>
2025-04-04 23:39:30 -0400

    update

--- lib/getData.ts
+++ lib/getData.ts
@@ -4,7 +4,8 @@ export default async function getData(): Promise<ObjectProps[]> {
     const API_KEY = process.env.HARVARD_ART_MUSEUM_API
     try {
         const page = Math.floor(Math.random() * 20000) + 1; // for random page every refresh
-        const response = await fetch(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&size=9&page=${page}`);
+        const response = await fetch(`https://api.harvardartmuseums.org/object?apikey=${API_KEY}&size=9&page=${page}`,
+            { cache: "no-store" });
         const data = await response.json();
         return data.records;
     }
